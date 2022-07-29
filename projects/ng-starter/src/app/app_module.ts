5e7c6417147202f2991c06bc88193e4385dc6211
Aurelian Gaiu <aurelian.gaiu11@gmail.com>
2022-07-29 11:38:30 +0300

    layout changes and improvement

--- projects/ng-starter/src/app/app_module.ts
+++ projects/ng-starter/src/app/app_module.ts
@@ -4,10 +4,8 @@ import { AppRoutingModule } from './app_routing_module';
 import { AppComponent } from './app';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarNavigationModule } from './core/components/sidebar-navigation/sidebar_navigation_module';
-
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, SidebarNavigationModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
